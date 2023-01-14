adc784fcc8f8150c730f64aaa8007de55675e4eb
jaredvance26 <jaredkvance@gmail.com>
2023-01-14 14:42:09 -0700

    lesson 2 study complete

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const routes = require("express").Router();
 const data = require("../controllers/");
 
+const myController = require("../controllers");
+
 routes.get("/", data.displayData);
+routes.get("/mongoData", myController.getMongoData);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const mongodb = require("./db/connection");
 
 app.use("/", require("./routes"));
 
-app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Running on port ${port}`);
+    });
+  }
 });
