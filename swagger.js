708f3deb629908a30aafa69af5f11445f7a37501
jaredvance26 <jaredkvance@gmail.com>
2023-01-27 17:05:09 -0700

    added node modules

--- swagger.js
+++ swagger.js
@@ -14,8 +14,3 @@ const endpointsFiles = ["./routes/index.js"];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
-
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import("./index.js");
-// });
